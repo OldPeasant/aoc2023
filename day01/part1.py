9865a5b8e67c9f23bd000d9c2d49b7a774f6f674
Peter Gürtler <coder@sonensei.ch>
2023-12-01 09:31:03 +0100

    Code cleanup day 01

--- day01/part1.py
+++ day01/part1.py
@@ -1,18 +1,8 @@
 import sys
 
-sys.path.append('../lib')
-from pmg import *
-
 with open(sys.argv[1]) as f:
-    lines = f.read().splitlines()
-
     total = 0
-    for l in lines:
-        nums = []
-        for c in l:
-            if c.isnumeric():
-                nums.append(int(c))
-        if len(nums) > 0:
-            p = 10 * nums[0] + nums[-1]
-        total += p
+    for l in f.read().splitlines():
+        nums = [int(c) for c in l if c.isnumeric()]
+        total += 10 * nums[0] + nums[-1]
     print(total)
