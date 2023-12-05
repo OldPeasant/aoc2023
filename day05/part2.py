ac3a51db38b887bfc54135745dbd7436204e916a
Peter Gürtler <coder@sonensei.ch>
2023-12-05 21:46:04 +0100

    Day 05 Part II

--- day05/part2.py
+++ day05/part2.py
@@ -3,66 +3,77 @@ import sys
 sys.path.append('../lib')
 from pmg import *
 
+def calc_seeds_intervals(seeds_input):
+    intervals = []
+    for i in range(0, len(seeds_input), 2):
+        intervals.append( (seeds_input[i], seeds_input[i] + seeds_input[i+1]) )
+    intervals.sort(key=lambda a:a[0])
+    return intervals
+
 class Mapping:
     def __init__(self, mapping_lines):
         title = mapping_lines[0]
         elems = title.split(" ")[0].split("-")
         self.map_src = elems[0]
         self.map_dest = elems[2]
-        self.ranges = []
+        tmp_ranges = []
         for row in mapping_lines[1:]:
-            self.ranges.append(list([int(v) for v in row.split()]))
+            dest, src, length = [int(v) for v in row.split()]
+            i_lower = src
+            i_offset = dest - src
+            i_upper = i_lower + length
+            tmp_ranges.append( (i_lower, i_upper, i_offset) )
+        tmp_ranges.sort(key=lambda a:a[0])
+        tmp2_ranges = []
+        tmp2_ranges.append( ("low", tmp_ranges[0][0], 0) )
+        for index in range(len(tmp_ranges)-1):
+            tmp2_ranges.append(tmp_ranges[index])
+            tmp2_ranges.append( (tmp_ranges[index][1], tmp_ranges[index+1][0], 0) )
+        tmp2_ranges.append(tmp_ranges[-1])
+        tmp2_ranges.append( (tmp_ranges[-1][1], "high", 0) )
 
-    def map_value(self, v):
-        #print("    Mapping " + self.map_src + " -> " + self.map_dest)
+        self.ranges =  []
+        for r in tmp2_ranges:
+            if r[0] != r[1]:
+                self.ranges.append(r)
+            
+    def map_single(self, interval):
+        result = []
         for r in self.ranges:
-            if v >= r[1] and v < r[1] + r[2]:
-                mapped_value = r[0] + v - r[1] 
-                #print("      in range  "  + str(r) + " -> " + str(mapped_value))
-                return mapped_value
-        #print("      no mapping, keeping " + str(v))
-        return v
-
-    def __repr__(self):
-        l = []
-        l.append("Dest " + self.map_dest + ", Src " + self.map_src)
-        for r in self.ranges:
-            l.append(", ".join([str(a) for a in r]))
-        return "\n".join(l)
+            i_from = (interval[0] if r[0] == "low" else max(interval[0], r[0])) + r[2]
+            i_to = (interval[1] if r[1] == "high" else min(interval[1], r[1])) + r[2]
+            if i_from < i_to:
+                result.append( (i_from, i_to) )
+            
+        return result
 
 class MappingColl:
     def __init__(self):
         self.mappings = []
 
-    def find_mapping_src(self, src_type):
+    def find_mapping(self, src_type):
         for m in self.mappings:
             if m.map_src == src_type:
                 return m
         raise Exception("No mapping for " + src_type + " found.")
 
-def find_location(mapping_coll, res_type, v):
-    #print("  find location for " + str(v) + " of type " + res_type)
-    if res_type == "location":
-        #print("  it's already location, return " + str(v))
-        return v
-    mapping = mapping_coll.find_mapping_src(res_type)
-    #print("  Relevant mapping" + str(mapping))
-    mv = mapping.map_value(v)
-    #print("  mapped value is " + str(mv))
-    return find_location(mapping_coll, mapping.map_dest, mv)
-
-def calc_seeds(seeds_input):
-    all_seeds = []
-    for ix in range(0, len(seeds_input), 2):
-        for s in range(seeds_input[ix], seeds_input[ix] + seeds_input[ix + 1]):
-            all_seeds.append(s)
-    return all_seeds
+def transform(mapping_coll, input_intervals, name):
+    mapping = mapping_coll.find_mapping(name)
+    result_intervals = []
+    for ii in input_intervals:
+        mapped = mapping.map_single(ii)
+        result_intervals.extend(mapped)
+    result_intervals.sort(key=lambda a: a[0])
+    if mapping.map_dest == 'location':
+        return result_intervals
+    else:
+        return transform(mapping_coll, result_intervals, mapping.map_dest)
 
 with open(sys.argv[1]) as f:
     lines = f.read().splitlines()
 
     seeds_input = [int(s) for s in lines[0].split(": ")[1].split()]
-    calc_seeds(seeds_input)
+    seeds_intervals = calc_seeds_intervals(seeds_input)
     grouper = Grouper()
     for l in lines[2:]:
         if len(l) == 0:
@@ -72,20 +83,5 @@ with open(sys.argv[1]) as f:
     mapping_coll = MappingColl()
     for group in grouper.groups:
         mapping_coll.mappings.append(Mapping(group))
-
-    #for m in mapping_coll.mappings:
-    #    print("Mapping:")
-    #    print(m)
-
-    locations = []
-    all_seeds = calc_seeds(seeds_input)
-    print("Number of seeds: " + str(len(all_seeds)))
-    ix = 0
-    for s in calc_seeds(seeds_input):
-        ix += 1
-        print("Examine seed no " + str(ix) + ": " + str(s))
-        loc = find_location(mapping_coll, "seed", s) 
-        print("location is " + str(loc))
-        locations.append(loc)
-    print(locations)
-    print(min(locations))
+    result = transform(mapping_coll, seeds_intervals, "seed")
+    print(result[0][0])
