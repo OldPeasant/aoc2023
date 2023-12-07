83ba1af910c1719685ab2e724853fc1ac4a1e292
Peter Gürtler <peter.guertler@ausy.ch>
2023-12-07 11:18:45 +0100

    Code cleanup day 07

--- day07/part2.py
+++ day07/part2.py
@@ -18,26 +18,30 @@ def calc_strength(cards):
     dl = {}
     for c in cards:
         dl[c] = dl.get(c, 0) + 1
-    print(dl)
     count_matches = {}
     for card, count in dl.items():
-        print("cc", card, count)
         count_matches[count] = count_matches.get(count, 0) + 1
-    print(count_matches)
     if count_matches.get(5, 0) >= 1:
+        # Five of a kind
         s = 7
     elif count_matches.get(4, 0) >= 1:
+        # Four of a kind
         s = 6
     elif count_matches.get(3, 0) >= 1:
         if count_matches.get(2, 0) >= 1:
+            # Full house
             s = 5
         else:
+            # Three of a kind
             s = 4
     elif count_matches.get(2, 0) >= 2:
+        # Two pairs
         s = 3
     elif count_matches.get(2, 0) >= 1:
+        # One pair
         s = 2
     else:
+        # High card
         s = 1
     return s
 
@@ -60,37 +64,28 @@ def joker_combinations(cards):
 
 class Hand:
     def __init__(self, cards):
-        print(cards)
         self.cards = cards
         joker_strengths = []
         for jc in joker_combinations(cards):
             joker_strengths.append(calc_strength(jc))
         self.strength = max(joker_strengths)
 
-    def is_bigger_than(self, other_hand):
-        print("is bigger", self, other_hand)
+    def compare_to(self, other_hand):
         if self.strength > other_hand.strength:
-            print("", "yes, strengthi")
             return 1
         elif self.strength < other_hand.strength:
-            print("", "no, strengthi")
             return -1
         for c_me, c_other in zip(self.cards, other_hand.cards):
             if VALUES[c_me] > VALUES[c_other]:
-                print("", "yes", c_me, c_other)
                 return 1
             elif VALUES[c_me] < VALUES[c_other]:
-                print("", "no", c_me, c_other)
                 return -1
-        print("", "no")
         return 0
     def __repr__(self):
         return "".join(self.cards) + ": " + str(self.strength)
 
 def compare_hands(h1, h2):
-    print("compare", h1, h2)
-    c = h1[0].is_bigger_than(h2[0])
-    print("", c)
+    c = h1[0].compare_to(h2[0])
     return c
 
 with open(sys.argv[1]) as f:
@@ -100,13 +95,8 @@ with open(sys.argv[1]) as f:
     for l in lines:
         s = l.split()
         hands_with_bids.append( (Hand(list(s[0])), int(s[1])) )
-    print(hands_with_bids)
     hands_with_bids.sort(key=functools.cmp_to_key(compare_hands))
-    print(hands_with_bids)
     winnings = 0
     for ix, hwb in enumerate(hands_with_bids):
-        print(ix, "|", hwb)
-        w = (ix + 1) * hwb[1]
-        print(w, hwb)
-        winnings += w
+        winnings += (ix + 1) * hwb[1]
     print(winnings)
