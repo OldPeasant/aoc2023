0293004395c4768277015dcd9895a75a75d92cf2
Peter Gürtler <coder@sonensei.ch>
2023-12-09 09:07:48 +0100

    Day 04 code cleanup

--- day04/part1.py
+++ day04/part1.py
@@ -1,23 +1,10 @@
 import sys
 
-sys.path.append('../lib')
-from pmg import *
-
 with open(sys.argv[1]) as f:
-    lines = f.read().splitlines()
-
     score = 0
-    for l in lines:
-        caption, values = l.split(": ")
-        winning_str, having_str = values.split(" | ")
-        winning = [ int(n) for n in (s for s in winning_str.split(" ") if s)]
-        having = [ int(n) for n in (s for s in having_str.split(" ") if s)]
-        print(winning)
-        print(having)
+    for i, l in enumerate(f.read().splitlines()): 
+        winning, having = ([int(i) for i in parts.split()] for parts in l.split(": ")[1].split(" | "))
         count_match = len(list(n for n in having if n in winning))
-        print(count_match)
         if count_match > 0:
-            points = pow(2, count_match - 1)
-            print(points)
-            score += points
+            score += pow(2, count_match - 1)
     print(score)
--- day04/part2.py
+++ day04/part2.py
@@ -1,19 +1,10 @@
 import sys
 
-sys.path.append('../lib')
-from pmg import *
-
 with open(sys.argv[1]) as f:
     lines = f.read().splitlines()
-
     copies = [1] * len(lines)
     for i, l in enumerate(lines):
-        caption, values = l.split(": ")
-        winning_str, having_str = values.split(" | ")
-        winning = [ int(n) for n in (s for s in winning_str.split(" ") if s)]
-        having = [ int(n) for n in (s for s in having_str.split(" ") if s)]
-        count_match = len(list(n for n in having if n in winning))
-        for c in range(i + 1, i + 1 + count_match):
-            if c < len(copies):
-                copies[c] += copies[i]
+        winning, having = ([int(i) for i in parts.split()] for parts in l.split(": ")[1].split(" | "))
+        for c in range(i + 1, min(i + 1 + len(list(n for n in having if n in winning)), len(copies))):
+            copies[c] += copies[i]
     print(sum(copies))
