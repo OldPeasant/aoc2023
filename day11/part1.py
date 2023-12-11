1f942ba8fce7392920bd91fa70b2c830c232198f
Peter Gürtler <peter.guertler@ausy.ch>
2023-12-11 09:04:54 +0100

    Code cleanup day 11

--- day11/part1.py
+++ day11/part1.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append('../lib')
 from pmg import *
 
+FACTOR = 2
 with open(sys.argv[1]) as f:
     lines = f.read().splitlines()
 
@@ -24,32 +25,21 @@ with open(sys.argv[1]) as f:
     for i in range(len(grid_small)):
         if i not in rows_with_galaxy:
             empty_rows.append(i)
-    print(empty_rows)
-    print(empty_cols)
-    grid_big = []
-    for row_ix, row in enumerate(grid_small):
-        new_row = []
-        for col_ix, cell in enumerate(row):
-            new_row.append(cell)
-            if col_ix in empty_cols:
-                new_row.append(".")
-        grid_big.append(new_row)
-        if row_ix in empty_rows:
-            grid_big.append("." * (len(grid_small[0]) + len(empty_cols)))
-    print("\n".join("".join(r) for r in grid_big))
     galaxies = []
-    for row_ix, row in enumerate(grid_big):
+    for row_ix, row in enumerate(grid_small):
         for col_ix, cell in enumerate(row):
             if cell == "#":
                 galaxies.append( (row_ix, col_ix) )
-    print(galaxies)
     total_dist = 0
     for i, g1 in enumerate(galaxies):
         for j, g2 in enumerate(galaxies):
             if j > i:
                 d = manhattan_dist(g1, g2)
-                print(d)
-                total_dist += d
+                f1 = list(1 for c in empty_rows if c > min(g1[0], g2[0]) and c < max(g1[0], g2[0]))
+                d += FACTOR * (sum(f1))
+                f2 = list(1 for c in empty_cols if c > min(g1[1], g2[1]) and c < max(g1[1], g2[1]))
+                d += FACTOR * (sum(f2))
+                total_dist += d - len(f1) - len(f2)
 
     print(total_dist)
 
--- day11/part2.py
+++ day11/part2.py
@@ -7,49 +7,36 @@ FACTOR = 1000000
 with open(sys.argv[1]) as f:
     lines = f.read().splitlines()
 
-    grid_small = []
-    for l in lines:
-        grid_small.append(l)
+    grid = list(lines)
     rows_with_galaxy = set()
     cols_with_galaxy = set()
-    for row_ix, row in enumerate(grid_small):
+    for row_ix, row in enumerate(grid):
         for col_ix, cell in enumerate(row):
             if cell == '#':
                 rows_with_galaxy.add(row_ix)
                 cols_with_galaxy.add(col_ix)
     empty_rows = []
     empty_cols = []
-    for i in range(len(grid_small[0])):
+    for i in range(len(grid[0])):
         if i not in cols_with_galaxy:
             empty_cols.append(i)
-    for i in range(len(grid_small)):
+    for i in range(len(grid)):
         if i not in rows_with_galaxy:
             empty_rows.append(i)
-    print(empty_rows)
-    print(empty_cols)
     galaxies = []
-    for row_ix, row in enumerate(grid_small):
+    for row_ix, row in enumerate(grid):
         for col_ix, cell in enumerate(row):
             if cell == "#":
                 galaxies.append( (row_ix, col_ix) )
-    print(galaxies)
     total_dist = 0
     for i, g1 in enumerate(galaxies):
         for j, g2 in enumerate(galaxies):
             if j > i:
                 d = manhattan_dist(g1, g2)
-                print(g1, g2, empty_rows, empty_cols)
-                #print(list(1 for c in empty_rows if c > g1[0] and c < g2[0]))
-                #print(list(1 for c in empty_cols if c > g1[1] and c < g2[1]))
-                print(d)
                 f1 = list(1 for c in empty_rows if c > min(g1[0], g2[0]) and c < max(g1[0], g2[0]))
-                print(f1)
                 d += FACTOR * (sum(f1))
-                print(d)
                 f2 = list(1 for c in empty_cols if c > min(g1[1], g2[1]) and c < max(g1[1], g2[1]))
-                print(f2)
                 d += FACTOR * (sum(f2))
-                print(d)
                 total_dist += d - len(f1) - len(f2)
 
     print(total_dist)
