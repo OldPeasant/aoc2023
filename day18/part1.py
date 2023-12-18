f166c3a4a5b201675a54b71de6d9bcb85b9b7003
Peter Gürtler <coder@sonensei.ch>
2023-12-18 18:20:24 +0700

    Some code cleanup day 18

--- day18/part1.py
+++ day18/part1.py
@@ -3,6 +3,13 @@ import sys
 sys.path.append('../lib')
 from pmg import *
 
+HEX_VAL = {l:v for v, l in enumerate('0123456789abcdef')}
+
+def hex_to_dec(hex_str):
+    return HEX_VAL[hex_str[-1]] + 16 * hex_to_dec(hex_str[:-1]) if len(hex_str) > 0 else 0
+
+DIRECTIONS = "RDLU"
+
 DELTA = {
         "R": (1, 0),
         "L": (-1, 0),
@@ -10,125 +17,69 @@ DELTA = {
         "D": (0, 1)
 }
 
-CHANGER = {
-    "R" : "UD",
-    "L" : "UD",
-    "U" : "LR",
-    "D" : "LR"
-}
+def find_vertical_switchers(h_segments, y_coord):
+    matches = []
+    for s in h_segments:
+        min_y = min(s[0][1], s[1][1])
+        max_y = max(s[0][1], s[1][1])
+        if s[0][0] == s[1][0] and min_y <= y_coord and max_y >= y_coord:
+            matches.append(s[0][0])
+    matches.sort()
+    return matches
+
+def find_horizontallers(h_segments, y_split):
+    result = []
+    for s in h_segments:
+        if s[0][0] == s[1][0]:
+            if min(s[0][1], s[1][1]) < y_split and max(s[0][1], s[1][1]) > y_split:
+                result.append( (s[0][0], s[0][0], True) )
+        else:
+            if s[0][1] == y_split:
+                result.append( (min(s[0][0], s[1][0]), max(s[0][0], s[1][0]), s[2]) )
+    result.sort(key=lambda a: a[0])
+    return result
 
 with open(sys.argv[1]) as f:
-    lines = f.read().splitlines()
-
     instr = []
-    for l in lines:
-        d, far_str = l.split()[:2]
-        instr.append( (d, int(far_str)) )
-    print(instr)
-    if instr[0][0] == instr[-1][0]:
-        raise Exception("Unexpected for me")
-    BIG = 999999999999999999
-    min_x = BIG
-    max_x = -BIG
-    min_y = BIG
-    max_y = -BIG
+    for l in f.read().splitlines():
+        direction, count_str = l.split()[:2]
+        distance = int(count_str)
+        instr.append( (direction, distance) )
     h_segments = []
-    cx = 160
-    cy = 238
+    cx, cy = 0, 0
+    y_splitter_set = set()
+    y_splitter_set.add(0)
     for ix, i in enumerate(instr):
         d, l = i
-        print(d, l)
-        nx = cx + l * DELTA[d][0]
-        ny = cy + l * DELTA[d][1]
-        if nx < min_x:
-            min_x = nx
-        if nx > max_x:
-            max_x = nx
-        if ny < min_y:
-            min_y = ny
-        if ny > max_y:
-            max_y = ny
-        print("-", d)
+        nx, ny = cx + l * DELTA[d][0], cy + l * DELTA[d][1]
+        y_splitter_set.add(ny)
         if d in "UD":
-            print("Fall A")
             changes_in_out = True
         else:
-            print("Fall B")
             ud = instr[ix - 1] + instr[ix + 1 if ix < len(instr) - 2 else 0]
             changes_in_out = ("U" in ud) != ("D" in ud)
-            print("===", ud, changes_in_out)
         h_segments.append( [ (cx, cy), (nx, ny), changes_in_out ] )
         cx, cy = nx, ny
-    print("Range ({}, {}) - ({}, {})".format(min_x, min_y, max_x, max_y))
-    for h in h_segments:
-        print("SEG", h)
-    splitters = []
-    for ix, h in enumerate(h_segments):
-        s1, s2, changes = h
-        if s1[0] == s2[0]:
-            # vertical
-            y1 = s1[1]
-            y2 = s2[1]
-            for y in range(min(y1, y2) + 1, max(y1, y2)):
-                if not changes:
-                    raise Exception()
-                splitters.append( (s1[0], s1[0], y, changes) )
-        elif s1[1] == s2[1]:
-            # horizontal
-            x1 = s1[0]
-            x2 = s2[0]
-            splitters.append( (min(x1, x2), max(x1, x2), s1[1], changes) )
-
-    for s in splitters:
-        print("SPLITTER", s)
-    by_row = DictOfLists()
-    for s in splitters:
-        by_row.add(s[2], s)
-    for l in by_row.dict.values():
-        l.sort(key = lambda r: r[0])
-    count_inside = 0
-    count_all = 0
-    for y in range(min_y, max_y + 1):
-        row = ['.'] * (1 + max_x - min_x)
-        last_s = None
-        is_inside = False
-        for s in by_row.get(y): #splitters:
-            if is_inside:
-                for x in range(last_s[1] + 1, s[0]):
-                    if row[x] != '.':
-                        raise Exception()
-                    row[x] = 'i'
-                    count_all += 1
-            if s[2] == y:
-                for x in range(s[0], s[1] + 1):
-                    if row[x] != '.':
-                        raise Exception()
-                    count_all += 1
-                    row[x] = '#'
-            if s[3]:
-                is_inside = not is_inside
-            last_s = s
-            
-        #print(row)
-        print("".join(row)) #i"#" if c else "." for c in row))
-    #exit(0)
-    print("Das Finale")
-    for y in range(min_y, max_y + 1):
-        print(by_row.get(y))
-        inside = False
-        segs = by_row.get(y)
-        prev = None
-        rc = 0
-        for s in segs:
-            if inside:
-                rc += s[0] - prev[1] - 1
-            rc += s[1] - s[0] + 1
-            if s[2]:
-                inside = not inside
-            prev = s
-        print("Row", y, rc)
-        count_inside += rc
-    print("Total", count_inside)
-    print(count_all)
-# 88832 is wrong
-# 88973 is too low
+    y_splitters = sorted(y_splitter_set)
+    last_y_split = y_splitters[0]
+    total = 0
+    for y_split in y_splitters:
+        if y_split - last_y_split > 1:
+            x_switchers = find_vertical_switchers(h_segments, last_y_split + 1)
+            for i in range(0, len(x_switchers), 2):
+                w = x_switchers[i+1] - x_switchers[i] + 1
+                contrib = w * (y_split - last_y_split - 1)
+                total += contrib
+            total += 0
+        horizontallers = find_horizontallers(h_segments, y_split)
+        is_on = False
+        last_h = None
+        for h in horizontallers:
+            total += h[1] - h[0] + 1
+            if is_on:
+                total += h[0] - last_h[1] - 1
+            if h[2]:
+                is_on = not is_on
+            last_h = h
+        last_y_split = y_split
+    print(total)
--- day18/part2.py
+++ day18/part2.py
@@ -17,27 +17,13 @@ DELTA = {
         "D": (0, 1)
 }
 
-CHANGER = {
-    "R" : "UD",
-    "L" : "UD",
-    "U" : "LR",
-    "D" : "LR"
-}
-
 def find_vertical_switchers(h_segments, y_coord):
-    #print("find vertical switchers at y={}".format(y_coord))
     matches = []
     for s in h_segments:
-        #print("     check segment {}".format(s))
         min_y = min(s[0][1], s[1][1])
         max_y = max(s[0][1], s[1][1])
         if s[0][0] == s[1][0] and min_y <= y_coord and max_y >= y_coord:
             matches.append(s[0][0])
-            #print("         yes")
-            if s[2] is not True:
-                raise Exception()
-        #else:
-        #    print("         no")
     matches.sort()
     return matches
 
@@ -45,51 +31,29 @@ def find_horizontallers(h_segments, y_split):
     result = []
     for s in h_segments:
         if s[0][0] == s[1][0]:
-            # It's vertical
             if min(s[0][1], s[1][1]) < y_split and max(s[0][1], s[1][1]) > y_split:
-                # Vertical applies, always as changer
                 result.append( (s[0][0], s[0][0], True) )
         else:
-            if s[0][1] != s[1][1]:
-                raise Exception()
-            # It's horizontal
             if s[0][1] == y_split:
                 result.append( (min(s[0][0], s[1][0]), max(s[0][0], s[1][0]), s[2]) )
     result.sort(key=lambda a: a[0])
-
     return result
 
 with open(sys.argv[1]) as f:
-    lines = f.read().splitlines()
-
     instr = []
-    for l in lines:
-        ## Part 1
-        #direction, count_str = l.split()[:2]
-        #distance = int(count_str)
-        #instr.append( (direction, distance) )
-        # Part 2
+    for l in f.read().splitlines():
         alleged_col = l.split()[2:][0][2:-1]
         direction = DIRECTIONS[int(alleged_col[-1])]
         distance = hex_to_dec(alleged_col[:-1])
         instr.append( (direction, distance) )
-    print(instr)
-    if instr[0][0] == instr[-1][0]:
-        raise Exception("Unexpected for me")
-    BIG = 999999999999999999
-
     h_segments = []
-    cx = 0
-    cy = 0
+    cx, cy = 0, 0
     y_splitter_set = set()
     y_splitter_set.add(0)
     for ix, i in enumerate(instr):
         d, l = i
-        print(d, l)
-        nx = cx + l * DELTA[d][0]
-        ny = cy + l * DELTA[d][1]
+        nx, ny = cx + l * DELTA[d][0], cy + l * DELTA[d][1]
         y_splitter_set.add(ny)
-        print("-", d)
         if d in "UD":
             changes_in_out = True
         else:
@@ -97,51 +61,26 @@ with open(sys.argv[1]) as f:
             changes_in_out = ("U" in ud) != ("D" in ud)
         h_segments.append( [ (cx, cy), (nx, ny), changes_in_out ] )
         cx, cy = nx, ny
-    for h in h_segments:
-        print("SEG", h)
     y_splitters = sorted(y_splitter_set)
-    print("Y splitters:", y_splitters)
     last_y_split = y_splitters[0]
     total = 0
-    print("===============================================")
     for y_split in y_splitters:
         if y_split - last_y_split > 1:
-            print("An y-multi-thingy from {} to {}".format(last_y_split + 1, y_split - 1))
             x_switchers = find_vertical_switchers(h_segments, last_y_split + 1)
-            print("switchers: {}".format(x_switchers))
             for i in range(0, len(x_switchers), 2):
                 w = x_switchers[i+1] - x_switchers[i] + 1
-                print("   w={}".format(w))
                 contrib = w * (y_split - last_y_split - 1)
-                print("   contrib={}".format(contrib))
                 total += contrib
-            #x_changers = list( (seg[0][0], seg[1][0], seg[2]) for seg in h_segments if seg[0][1] == seg[1][1] and seg[0][1] in range(last_y_split, y_split) )
-            #print(x_changers)
-            # calc in-outs for row y_split-1, only considering vertical changes_in_out=True items
-            # multiply with y_split-last_y_split
-
-            # calc for row y_split
             total += 0
-            print()
-        print("A horizontaller at y={}".format(y_split))
         horizontallers = find_horizontallers(h_segments, y_split)
-        print("horizontallers={}".format(horizontallers))
-        row_total = 0
         is_on = False
         last_h = None
         for h in horizontallers:
-            row_total += h[1] - h[0] + 1
+            total += h[1] - h[0] + 1
             if is_on:
-                row_total += h[0] - last_h[1] - 1
+                total += h[0] - last_h[1] - 1
             if h[2]:
                 is_on = not is_on
             last_h = h
-        if is_on:
-            raise Exception()
-        print("   row_total = {}".format(row_total))
-        total += row_total
-        print()
-            
         last_y_split = y_split
     print(total)
-    exit(0)
